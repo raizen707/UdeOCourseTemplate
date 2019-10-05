2d38a991b02cfa0ef90b50ce2ef4f56db067e0a8
Raizen707 <Raizen707@RAIZEN-ROG>
2019-10-05 14:57:09 -0600

    ui for weather app, adding: layout string values navitagion menus

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -1,13 +1,57 @@
 package com.example.ui
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.udeocoursetemplate.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
+private const val MY_PERMISSION_ACCESS_COARSE_LOCATION =1
 class MainActivity : AppCompatActivity() {
+//LAZY LOAD VARIABLE
+    private  lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+        if (hasLoctaionPermission()){
+
+        }else{
+            requestLocaionPermission()
+        }
     }
+
+    private fun bindLocationManager(){
+        //LifeCycleBoundLocationManager()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+     return NavigationUI.navigateUp(navController,null)
+    }
+    private fun requestLocaionPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSION_ACCESS_COARSE_LOCATION
+        )
+    }
+    private fun hasLoctaionPermission(): Boolean{
+        return ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION)  == PackageManager.PERMISSION_GRANTED
+    }
+
 }
